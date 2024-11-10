6fe90e6e8faa6a0075f5aaca3fbc253cdfe86b0b
Andy Norris <anorris@atomicsemi.com>
2024-11-10 12:03:06 -0800

    Add test audio data and fix file path in lookup table

--- src/player/audio_lookup_table.v
+++ src/player/audio_lookup_table.v
@@ -4,17 +4,22 @@ module audio_lookup_table (
     input wire [13:0] address,
     output reg [15:0] data_out
 );
-    reg [15:0] rom [0:16383];
+    reg [15:0] rom [0:16383];  // 16K samples
 
     initial begin
-        $readmemh("audio_data.hex", rom);
+        if (!$value$plusargs("ROM=%s", rom_file)) begin
+            $readmemh("../src/player/audio_data.hex", rom);
+        end else begin
+            $readmemh(rom_file, rom);
+        end
     end
 
     always @(*) begin
-        if ({2'b00, address} < 16384) begin  // Zero-extend address
+        if ({2'b00, address} < 16384) begin
             data_out = rom[address];
         end else begin
             data_out = 16'd0;
         end
     end
+
 endmodule
